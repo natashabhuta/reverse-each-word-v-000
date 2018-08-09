dd6b73f2d03ef2c74c1bbb9279a97971dbfa75f8
natashabhuta <natasha.bhuta@gmail.com>
2018-08-09 18:36:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
  words_array = sentence.split(" ")
  new_array = []
- new_array = words_array.collect do |words|
+ new_array = words_array.collect do |word|
    words.reverse
    new_array.join
  end 
